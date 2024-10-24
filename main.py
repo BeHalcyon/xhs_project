8768b4eaade4b761465b5ddbdcb7ae45c6ef0318
hexiangyang <hexiangyang@corp.netease.com>
2024-10-24 17:07:15 +0800

    .

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def comment_test():
     cookie = "a1=18d5fae5576zwcukigljfy0aaw0ady2k1e847m5ph30000294564; webId=e11488368087c8ef20861064f4caaa7c; gid=yYf2i0d2DDyDyYf2i0d22d62WKuES7T3k1ViYUxv800E84q80fvJTW888Jj42K48Dq4Kd4dJ; abRequestId=e11488368087c8ef20861064f4caaa7c; webBuild=4.39.0; web_session=0400698ed6dc9256561e2a222d354ba12b8619; customer-sso-sid=68c517428807402125437553b99877216c27ffc6; x-user-id-creator.xiaohongshu.com=5fa29fda0000000001000648; customerClientId=616596286637831; access-token-creator.xiaohongshu.com=customer.creator.AT-68c5174288074021254375540vzakx5ppofte9mg; galaxy_creator_session_id=GSUX3DrW3ZfZHdeYVF6dwMQ7mWBaTTXgWBDh; galaxy.creator.beaker.session.id=1729654009085019524796; acw_tc=1824b0a96f9c5b8241104e795345716176374d70bc029838396fb3b90bb9f5ee; xsecappid=xhs-pc-web; websectiga=82e85efc5500b609ac1166aaf086ff8aa4261153a448ef0be5b17417e4512f28; sec_poison_id=2595d4dd-7d59-40b9-baa7-74e221d0d320; unread={%22ub%22:%22670714af000000002c0164ee%22%2C%22ue%22:%226707ce40000000001902d58b%22%2C%22uc%22:12}"  # put your cookie here
 
     # 16638143970
-    cookie = "abRequestId=14794d6b-b799-5f27-af00-cea84bc4bfff; webBuild=4.40.2; xsecappid=xhs-pc-web; a1=192ba09760cxdltwopebh57q5djf4pbjqsqtv41yi50000226449; webId=d17a290f39792fe352318aae94eb9878; websectiga=29098a4cf41f76ee3f8db19051aaa60c0fc7c5e305572fec762da32d457d76ae; sec_poison_id=07dabe4b-7368-4357-b502-7fab538a4de1; acw_tc=52f221185a8fe4154af8d27529f7cbc3b13cb6c21228e6964797149caa3a64b9; gid=yjJD08jY8SAqyjJD08jWKM4y8SCf19ElUdDxAv32WA2fV928i4UDVh888JJK44j8y4fKWJiq; web_session=0400698e590ea3566e3949512c354bcaffc385; unread={%22ub%22:%226713cdae0000000016023c83%22%2C%22ue%22:%2267166d6b000000002100804c%22%2C%22uc%22:28"
+    # cookie = "abRequestId=14794d6b-b799-5f27-af00-cea84bc4bfff; webBuild=4.40.2; xsecappid=xhs-pc-web; a1=192ba09760cxdltwopebh57q5djf4pbjqsqtv41yi50000226449; webId=d17a290f39792fe352318aae94eb9878; websectiga=29098a4cf41f76ee3f8db19051aaa60c0fc7c5e305572fec762da32d457d76ae; sec_poison_id=07dabe4b-7368-4357-b502-7fab538a4de1; acw_tc=52f221185a8fe4154af8d27529f7cbc3b13cb6c21228e6964797149caa3a64b9; gid=yjJD08jY8SAqyjJD08jWKM4y8SCf19ElUdDxAv32WA2fV928i4UDVh888JJK44j8y4fKWJiq; web_session=0400698e590ea3566e3949512c354bcaffc385; unread={%22ub%22:%226713cdae0000000016023c83%22%2C%22ue%22:%2267166d6b000000002100804c%22%2C%22uc%22:28"
 
     print_format("load comment...")
 
     ## 根据分类刷文章
-    articles = article_list_request(cookie, category=FeedType.EMOTION.value)
+    articles = article_list_request(cookie, category=FeedType.TRAVEL.value)
     # "homefeed.love_v3"
     print_format(f"All articles count: {len(articles)}")
     for index, item in enumerate(articles):
@@ -48,7 +48,7 @@ def comment_test():
         print_format(f"[{index+1}/{len(articles)}] ready to comment...")
         comment_response = comment_request(cookie, note_id, target_comment_id, target_comment, at_users=[])
         print_format(f"[{index+1}/{len(articles)}] comment result: {comment_response}")
-        sleep_time = random.randint(60, 80)
+        sleep_time = random.randint(60, 150)
         print_format(f"[{index+1}/{len(articles)}] sleep time: {sleep_time}s.")
         time.sleep(sleep_time)
 
--- xhs_interfaces/xhs_interface.py
+++ xhs_interfaces/xhs_interface.py
@@ -14,28 +14,46 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def comment_request(cookie, note_id, target_comment_id, content, at_users=[]):
     url = "https://edith.xiaohongshu.com/api/sns/web/v1/comment/post"
 
+    # headers = {
+    #     'accept': 'application/json, text/plain, */*',
+    #     'accept-language': 'zh-CN,zh;q=0.9',
+    #     'origin': 'https://www.xiaohongshu.com',
+    #     'priority': 'u=1, i',
+    #     'referer': 'https://www.xiaohongshu.com/',
+    #     'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+    #     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+    #     'content-type': 'application/json;charset=UTF-8'
+    # }
     headers = {
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'zh-CN,zh;q=0.9',
         'origin': 'https://www.xiaohongshu.com',
         'priority': 'u=1, i',
         'referer': 'https://www.xiaohongshu.com/',
-        'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+        'x-b3-traceid': '92008d787b241500',
+        'x-s-common': '2UQAPsHCPUIjqArjwjHjNsQhPsHCH0rjNsQhPaHCH0P1+UhhHjIj2eHjwjQ+GnPW/MPjNsQhPUHCHdYiqUMIGUM78nHjNsQh+sHCH0c1+eZ1PUHVHdWMH0ijP/YD+n8Y8/LM+A8C49+My9SdJBkf2/mYGgqIGnzEPfVl8/WF+9FMqBWAPeZIPeHE+eL9+sHVHdW9H0il+AHE+ALEw/DU+0LUNsQh+UHCHSY8pMRS2LkCGp4D4pLAndpQyfRk/SzpyLleadkYp9zMpDYV4Mk/a/8QJf4EanS7ypSGcd4/pMbk/9St+BbH/gz0zFMF8eQnyLSk49S0Pfl1GflyJB+1/dmjP0zk/9SQ2rSk49S0zFGMGDqEybkea/8QJLEingkByFMCpgSyzbQx/nkbPrEon/b+pM8T/fk34FRLc/+yJL8T/0QtJpSga/QwyfVF/Fz82bSxcfk8PSk3/fkpPDMTp/z82DrFnp4Q+rRrpfM+PD8i/nk0PLRLafTw2fqI/MzVJrFULgSwzBlx/dk+PMkLLfT8yfzVnfk8+rMrG7k+yDrAnSznJrRozfT8PSpC/pzdPrEr8AbwprQV//QQPrRLyBkwpMrU/Lzp2rMrLfY+2DkV/Lz8PDMgngk+zBTh/fkp2rFULfS+PSrA/D4ayMSxLfl+zMbCnnkiJrMgng4+zb83/pzp+LRgaflyySkx/L4tJpSgzfM8ySSE/DztyFhUngSOpFkVnp4typDUn/+8pB+C/fkbPMSLcfM8prEi/gkbPDRLG7Sypr8V/gkz2pSLn/++zbrUnDz32rEL/fYOpFMh/pzayMkxLfT+pb8x/MztJLMo//m+zrkkngkmPpSgng4+zBql/MzayLMLa/pwpBT7nSzz2rEop/zOzBz3/fMQ2rhUnfYypM8T//QwyDEgafl+pbrl/nM+4FRgp/++pFkTnfk0PpSLLfY8JLMCnfMpPDRrzfYyprkx/dk+2bSxp/byzBzx/dkpPDMCp/z8JLkx/p4+PFEL8Bk8PSLI/fMQ2LhUp/+wySLA//QaJpkxngk+2fVM/pzzPDECpfY+JpSCnp4Q2Skr/g4OzbSEnnM+2rMgLgS+prDUnDzz4FMoafS+zbDUanhIOaHVHdWhH0ija/PhqDYD87+xJ7mdag8Sq9zn494QcUT6aLpPJLQy+nLApd4G/B4BprShLA+jqg4bqD8S8gYDPBp3Jf+m2DMBnnEl4BYQyrkSzeS+zrTM4bQQPFTAnnRUpFYc4r4UGSGILeSg8DSkN9pgGA8SngbF2pbmqbmQPA4Sy9MaPpbPtApQy/8A8BE68p+fqpSHqg4VPdbF+LHIzBRQ2sV3zFznN7+n4BTQ2BzA2op7q0zl4BSQy7Q7anD6q9T0GA+QPM89aLP7qMSM4MYlwgbFqr898Lz/ad+/Lo4GaLp9q9Sn4rkOLoqhcdp78SmI8BpLzb4OagWFpDSk4/byLo4jLopFnrS9JBbPGf4AP7bF2rSh8gPlpd4HanTMJLS3agSSyf4AnaRgpB4S+9p/qgzSNFc7qFz0qBSI8nzSngQr4rSe+fprpdqUaLpwqM+l4Bl1Jb+M/fkn4rS9J9p3qg4+89QO8/bSn/QQzp+canYi8MbDappQPAYc+BMCtFSkG9SSLocMaL+y4/zs/d+rGFEAyM87pDSeJ9pD+A4Szo+M8FSk8BL9zSka2gpFzDSiN7P98/pA2bk98p8n47b0c/4SzopFwLS9prlPNFRSPob7cFlsa7+fqg4Aa/+LaFDApb+U4g4ANM87z7+SG9QzGf4AyDbOq7Yl4B4QyBpAyfRS8nT0nL+QzaRS8bk98/PItA4QyLkSyp8F/LSbpflO4g4tLgbFpFSeN7PA4g4eaLpO8nTn4ApQye+Aydp7+LEl49E6pd4MaL+0cFShzrlt8Dzr+bqM8/+M4e4QyrYEanSm8nkl4Flj2dkCqoPMq9kpL9TQyn+SqpmFaBRl49YQ4DbSPM8FyMmU8nLlnLTAprzV8LSiadPA804AyMmF8Flc49RQ4DRSPob7cLSk4fpnNFSSag8d8p+6P9prqgzQagW7qM8c49To+UT6aLplqLS9/dP9zepSy94S8nzyJ7+rpdzFanSBwLShy/SdLo4cagYPzDSb+9p8Pb4/a0DM8nkM47mQzL8TGdpFpAQPPo+rLA4SL7p7t9Rc4URQcMr7anTkLgmn47kopdzwaL+98n8I+d+3LozSndb7arSkGAQdpd4VagYVG7Z7/LRQyo8S8SL98LzA/d+nLoz6+op7/DSbL0YQ4DkSP7b7yDSk/7PIap88a/+UqBM+4pbQPM4Ca/P98/mc49ESpdzAHjIj2eDjw0rUPALIPeGMP0GVHdWlPsHCP0YR',
+        'x-xray-traceid': 'c95edd6da5f9962343c341e6d9c4b3b7',
+        # 'Cookie': 'a1=18d5fae5576zwcukigljfy0aaw0ady2k1e847m5ph30000294564; webId=e11488368087c8ef20861064f4caaa7c; gid=yYf2i0d2DDyDyYf2i0d22d62WKuES7T3k1ViYUxv800E84q80fvJTW888Jj42K48Dq4Kd4dJ; abRequestId=e11488368087c8ef20861064f4caaa7c; web_session=0400698ed6dc9256561e2a222d354ba12b8619; customer-sso-sid=68c517428807402125437553b99877216c27ffc6; x-user-id-creator.xiaohongshu.com=5fa29fda0000000001000648; customerClientId=616596286637831; access-token-creator.xiaohongshu.com=customer.creator.AT-68c5174288074021254375540vzakx5ppofte9mg; galaxy_creator_session_id=GSUX3DrW3ZfZHdeYVF6dwMQ7mWBaTTXgWBDh; galaxy.creator.beaker.session.id=1729654009085019524796; xsecappid=xhs-pc-web; unread={%22ub%22:%22671772140000000021008975%22%2C%22ue%22:%226717bba5000000001402ed5b%22%2C%22uc%22:29}; webBuild=4.40.3; websectiga=82e85efc5500b609ac1166aaf086ff8aa4261153a448ef0be5b17417e4512f28; sec_poison_id=24eb7c17-fbad-4e53-b571-ba5e2c673707; acw_tc=481305b34d7195217425d438611ae391cdd758a5ebc17dd81cf6740dab28d4d0',
         'content-type': 'application/json;charset=UTF-8'
     }
 
     payload = {
         "note_id": note_id,
         "content": content,
-        "target_comment_id": target_comment_id,
+        # "target_comment_id": target_comment_id,
         "at_users": at_users
     }
+    if target_comment_id:
+        payload['target_comment_id'] = target_comment_id
     api = url[url.find("/api"):]
 
     xs_xt = parse_result(api, cookie, payload)
 
-    headers['cookie'] = cookie
+    headers['Cookie'] = cookie
     headers['X-s'] = xs_xt['X-s']
     headers['X-t'] = str(xs_xt['X-t'])
 
